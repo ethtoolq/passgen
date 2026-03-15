ee7b7de8c9e137e2e0dbe0ce7af9ccbe31f213c9
eth <ethtoolq@proton.me>
2026-03-15 00:16:00 +0000

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -4,7 +4,7 @@ import string
 from datetime import datetime
 
 
-# генерация случайного пароля
+# random passgen
 def gen_password(length: int) -> str:
     alphabet = string.ascii_letters + string.digits
     return ''.join(secrets.choice(alphabet) for _ in range(length))
@@ -14,26 +14,26 @@ def main():
     parser = argparse.ArgumentParser(description="Random Password Generator")
 
     parser.add_argument("-n", "--number", type=int, default=10,
-                        help="Количество паролей")
+                        help="Number of passwords")
     parser.add_argument("-l", "--length", type=int, default=16,
-                        help="Длина пароля")
+                        help="Password length")
     parser.add_argument("-o", "--out",
                         default=f"passwords_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt",
-                        help="Файл для сохранения")
+                        help="File to save")
 
     args = parser.parse_args()
 
-    # проверка параметров
+    # parameter check
     if args.number < 1 or args.length < 1:
         print("Параметры должны быть >= 1")
         return
 
-    # генерация и запись паролей
+    # generate and record passwords
     with open(args.out, "w", encoding="utf-8") as f:
         for _ in range(args.number):
             f.write(gen_password(args.length) + "\n")
 
-    print(f"Сгенерировано {args.number} паролей. Файл: {args.out}")
+    print(f"Generated {args.number} password. File: {args.out}")
 
 
 if __name__ == "__main__":
